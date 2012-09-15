4b01c3ba60b58063692d3002feb124e075dff673
root <root@mehdi-HP-EliteBook-8530w.(none)>
2012-09-15 13:34:20 +0200

    Facebook connect 2/2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
   # attr_accessible :title, :body
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+    end
+    user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Wikitrip::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get "home/index"
-
 
   root :to => "home#index"
   # The priority is based upon order of creation:
