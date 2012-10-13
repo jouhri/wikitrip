865b231eb8067f5dd761f9a3de23efee7beed9ef
oualid <oualid@oualid-HP-EliteBook-8530w.(none)>
2012-10-13 15:04:47 +0800

    ajout du marker sur la map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
   	result = request.location
+  	@lat = result.latitude
+  	@long = result.longitude
   	puts "==========================================================================================================="
   	puts result.inspect
   end
