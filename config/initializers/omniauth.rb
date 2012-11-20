c99555bac33f3e8e87a890c9d8853df92fd6220a
oualid <oualid@oualid-HP-EliteBook-8530w.(none)>
2012-11-20 18:51:22 +0800

    ajout de la facebook key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['252392681538023'], ENV['1655c2ec98cf14ffd6ee80960ff4511a']
 end
